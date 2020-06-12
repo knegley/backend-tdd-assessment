6111af7d0f46684c703841a6b8c4647f53397814
knegley <kpnegley@gmail.com>
2020-06-12 12:31:52 -0400

    got echo command to work

--- echo.py
+++ echo.py
@@ -2,23 +2,67 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "Kyle Negley"
 
 
-import sys
+# import sys
+import argparse
 
 
 def create_parser():
-    """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    """Returns namespace object"""
+    parser = argparse.ArgumentParser(
+        description="prints input and includes string methods", usage="echo.py[-h][-u][-l][-t] text")
+    parser.add_argument(
+        "-l", "--lower", action="store_true", help="lower case all characters")
+    parser.add_argument(
+        "-u", "--upper", action="store_true", help="upper case all characters")
+    parser.add_argument(
+        "-t", "--title", action="store_true", help="makes text titlecase")
+    parser.add_argument(
+        "text", help="echo.py <text> to print <text>")
+
+    return parser.parse_args()
 
 
-def main(args):
+def text_manipulater(command):
+    args = create_parser()
+    return{
+        "lower": lambda: args.text.lower(),
+        "upper": lambda: args.text.upper(),
+        "title": lambda: args.text.title()
+    }.get(command, lambda: args.text)()
+
+
+def echo_Output():
+    args = create_parser()
+    # print(args.__dict__)
+
+    for arg, value in args.__dict__.items():
+        if arg != "text" and value:
+            return(text_manipulater(arg))
+    return text_manipulater(None)
+
+    # if args.lower is not None:
+    #     print(args)
+    #     print(lamba_dict["lower"]())
+
+    # print(lambda_dict.get(None, lambda: args.text)())
+
+    # if args.lower:
+    #     print(lambda_dict["lower"]())
+    # elif args.upper:
+    #     print(lambda_dict["upper"]())
+    # elif args.title:
+    #     print(lambda_dict["title"]())
+    # else:
+    #     print(lambda_dict.get(None, lambda: args.text)())
+
+
+def main():
     """Implementation of echo"""
-    # your code here
-    return
+    print(echo_Output())
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -7,7 +7,7 @@ Students are expected to edit this module, to add more tests to run
 against the 'echo.py' program.
 """
 
-__author__ = "???"
+__author__ = "Kyle Negley"
 
 import sys
 import importlib
@@ -25,6 +25,7 @@ PKG_NAME = 'echo'
 # Students can use this class object in their code
 class Capturing(list):
     """Context Mgr helper for capturing stdout from a function call"""
+
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
@@ -47,7 +48,7 @@ def run_capture(pyfile, args=()):
     p = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+    )
     stdout, stderr = p.communicate()
     stdout = stdout.decode().splitlines()
     stderr = stderr.decode().splitlines()
@@ -69,8 +70,8 @@ class TestEcho(unittest.TestCase):
         cls.funcs = {
             k: v for k, v in inspect.getmembers(
                 cls.module, inspect.isfunction
-                )
-            }
+            )
+        }
         # check the module for required functions
         assert "main" in cls.funcs, "Missing required function main()"
         assert "create_parser" in cls.funcs, "Missing required function create_parser()"
@@ -104,7 +105,7 @@ class TestEcho(unittest.TestCase):
         self.assertEqual(
             stdout[0], args[0],
             "The program is not performing simple echo"
-            )
+        )
 
     def test_lower_short(self):
         """Check if short option '-l' performs lowercasing"""
